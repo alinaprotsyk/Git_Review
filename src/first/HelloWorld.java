f58888e211a85e4db2e083e9d3b28ab5b48495a2
alinaprotsyk <alinaprotsyk2108@gmail.com>
2022-08-23 11:31:42 -0400

    HelloWorld.java changes added

--- src/first/HelloWorld.java
+++ src/first/HelloWorld.java
@@ -3,5 +3,8 @@ package first;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("Maybe 5 weeks of Java left");
+        System.out.println("1.5 weeks of git");
     }
 }
